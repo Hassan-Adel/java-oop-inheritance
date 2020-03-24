21922263a3f1ba84f940387568e095b0ba360a2f
hassan.adel <hasanadel555@hotmail.com>
2020-03-24 23:27:07 +0200

    upcasting and downcasting

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -4,10 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        var control = new UIControl(true);
         var box1 = new Textbox();
         box1.setText("Hello");
+        show(control);
 
-        System.out.println(box1.toString());
-        System.out.println(box1);
+        //Upcasting (every Textbox IS A UIControl)
+        show(box1);
+    }
+    public static void show(UIControl uiControl){
+        if(uiControl instanceof Textbox){
+            //DownCasting
+            var textBox = (Textbox)uiControl;
+            textBox.setText("Hello after Downcasting");
+        }
+        System.out.println(uiControl);
     }
 }
