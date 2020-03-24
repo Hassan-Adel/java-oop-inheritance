447067489bfe81d833345b4958ea89113bd09ccc
hassan.adel <hasanadel555@hotmail.com>
2020-03-24 21:49:10 +0200

    examining the Object class

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -4,8 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        var control = new Textbox();
-        control.disable();
-        System.out.println(control.isEnabled() );
+        var box1 = new Textbox();
+        var box2 = box1;
+        var box3 = new Textbox();
+        System.out.println(box1.hashCode());
+        System.out.println(box2.hashCode());
+        // will have a diff. hashcode cuz it's in a diff. address
+        System.out.println(box3.hashCode());
+
+        System.out.println(box1.toString());
+        System.out.println(box1.equals(box2));
+        System.out.println(box1.equals(box3));
     }
 }
