f85a7ee3fa0eed936125eaa837244656d952dcb1
hassan.adel <hasanadel555@hotmail.com>
2020-03-25 00:02:20 +0200

    Comparing objects (override equals and hashCode)

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        var control = new UIControl(true);
-        var box1 = new Textbox();
-        box1.setText("Hello");
-        show(control);
+        var point1 = new Point(1,2);
+        var point2 = new Point(1,2);
+        System.out.println(point1.equals(point2));
+        System.out.println(point1 == point2);
 
-        //Upcasting (every Textbox IS A UIControl)
-        show(box1);
+        System.out.println(point1.hashCode());
+        System.out.println(point2.hashCode());
     }
     public static void show(UIControl uiControl){
         if(uiControl instanceof Textbox){
